6ac9af9ab0b2b8ce43ad2f569ef10e108d443a57
Koichi Okutsu <okutsu@pej.jp>
2019-05-23 20:45:45 +0900

    [change] delete console.log

--- docs/index.js
+++ docs/index.js
@@ -2,7 +2,6 @@
     window.addEventListener('DOMContentLoaded', function () {
         while (document.getElementsByClassName('keyword').length > 0) {
             Array.prototype.forEach.call(document.getElementsByClassName('keyword'), function (e) {
-                console.log(e);
                 var nn = document.createTextNode(e.innerHTML);
                 e.parentNode.replaceChild(nn, e);
             });
